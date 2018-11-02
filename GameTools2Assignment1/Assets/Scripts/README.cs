8c185f8e51d5f2c97ba88441aec371d676ca478e
IIMakII <37370335+IIMakII@users.noreply.github.com>
2018-11-02 17:18:23 +0000

    README FILE

---
+++ GameTools2Assignment1/Assets/Scripts/README.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour {
+
+    
+    //  OLUWAMYOWA AKINOLA ANIMATION PROJECT
+    // ANIMATIONS AND MODEL TAKEN OFF MIXAMO.COM
+    // I MADE THIS PROJECT TO BE PLAYED USING A XBOX 360 CONTROLLER
+   //  IF THIS IS NOT USE SOMETHINGS WOULD NOT WORK AS INTENDED OR FULL MECHANICS WOULD NOT BE UTILISED
+
+      
+   }
+
+  
